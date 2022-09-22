da83f635b0586606389c45ee7c1cfb9e925033ce
Dragonspine <ghoul300@naver.com>
2022-09-22 23:12:15 +0900

    feat: Bean Validation V3 groups

---
+++ src/main/java/hello/itemservice/domain/item/UpdateCheck.java
@@ -0,0 +1,15 @@
+package hello.itemservice.domain.item;
+
+/**
+ * packageName    : hello.itemservice.domain.item
+ * fileName       : UpdateCheck
+ * author         : kanghyun Kim
+ * date           : 2022/09/22
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/09/22        kanghyun Kim      최초 생성
+ */
+public interface UpdateCheck {
+}
